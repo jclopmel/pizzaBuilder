6b20d439782d01c7465a67d4b3cdd566d55b64d4
Javier Arenas Iglesias <javierareigl@gmail.com>
2017-07-13 18:09:24 +0200

    some errors fixed (deleted classes in the HTML)

--- js/pizza.js
+++ js/pizza.js
@@ -1,23 +1,29 @@
 $('.btn-pepperonni').click(function(){ //fade de pepperonni en la lista de la derecha al pulsar el botón
-        $('.pepperonni').fadeToggle();
+        var pepperonni = $('li')[5];
+        $(pepperonni).fadeToggle();
         $('.pep').fadeToggle();
       });
 
 $('.btn-mushrooms').click(function(){ //fade de mushrooms en la lista de la derecha al pulsar el botón
-        $('.mushrooms').fadeToggle();
+        var mushrooms = $('li')[6];
+        $(mushrooms).fadeToggle();
         $('.mushroom').fadeToggle();
       });
 
 $('.btn-green-peppers').click(function(){ //fade de green-peppers en la lista de la derecha al pulsar el botón
-        $('.green-peppers').fadeToggle();
+        var greenPeppers = $('li')[7];
+        $(greenPeppers).fadeToggle();
         $('.green-pepper').fadeToggle();
       });
 
 $('.btn-sauce').click(function(){ //fade de sauce en la lista de la derecha al pulsar el botón
-        $('.sauces').fadeToggle();
-        $('.sauce').fadeToggle();
+        var sauce = $('li')[8];
+        $(sauce).fadeToggle();
+        $('.sauce').toggleClass('sauce-white');
       });
 
 $('.btn-crust').click(function(){ //fade de pepperonni en la lista de la derecha al pulsar el botón
-        $('.cruste').fadeToggle();
+        var crust = $('li')[9];
+        $(crust).fadeToggle();
+        $('.crust').toggleClass('crust-gluten-free');
       });
