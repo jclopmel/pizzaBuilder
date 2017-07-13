d3f586f67c33f0c03cdbd30b895075e26e540451
Javier Arenas Iglesias <javierareigl@gmail.com>
2017-07-13 17:41:30 +0200

    added animation to the pizza

--- js/pizza.js
+++ js/pizza.js
@@ -1,17 +1,21 @@
 $('.btn-pepperonni').click(function(){ //fade de pepperonni en la lista de la derecha al pulsar el botón
         $('.pepperonni').fadeToggle();
+        $('.pep').fadeToggle();
       });
 
 $('.btn-mushrooms').click(function(){ //fade de mushrooms en la lista de la derecha al pulsar el botón
         $('.mushrooms').fadeToggle();
+        $('.mushroom').fadeToggle();
       });
 
 $('.btn-green-peppers').click(function(){ //fade de green-peppers en la lista de la derecha al pulsar el botón
         $('.green-peppers').fadeToggle();
+        $('.green-pepper').fadeToggle();
       });
 
 $('.btn-sauce').click(function(){ //fade de sauce en la lista de la derecha al pulsar el botón
         $('.sauces').fadeToggle();
+        $('.sauce').fadeToggle();
       });
 
 $('.btn-crust').click(function(){ //fade de pepperonni en la lista de la derecha al pulsar el botón
