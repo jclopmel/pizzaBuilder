a9edd323d67fdb7b28be3e6d3b05bac4dd991c9a
Javier Arenas Iglesias <javierareigl@gmail.com>
2017-07-13 17:33:19 +0200

    added fade

--- js/pizza.js
+++ js/pizza.js
@@ -1,1 +1,19 @@
-// Write your Pizza Builder JavaScript in this file.
+$('.btn-pepperonni').click(function(){ //fade de pepperonni en la lista de la derecha al pulsar el botón
+        $('.pepperonni').fadeToggle();
+      });
+
+$('.btn-mushrooms').click(function(){ //fade de mushrooms en la lista de la derecha al pulsar el botón
+        $('.mushrooms').fadeToggle();
+      });
+
+$('.btn-green-peppers').click(function(){ //fade de green-peppers en la lista de la derecha al pulsar el botón
+        $('.green-peppers').fadeToggle();
+      });
+
+$('.btn-sauce').click(function(){ //fade de sauce en la lista de la derecha al pulsar el botón
+        $('.sauces').fadeToggle();
+      });
+
+$('.btn-crust').click(function(){ //fade de pepperonni en la lista de la derecha al pulsar el botón
+        $('.cruste').fadeToggle();
+      });
