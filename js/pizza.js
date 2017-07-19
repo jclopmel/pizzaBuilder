05cdac9e4b309d7ef19a7e38ddf4ef8961c9da05
jclopmel <jclm1988@gmail.com>
2017-07-19 18:57:12 +0200

    finished

--- js/pizza.js
+++ js/pizza.js
@@ -1,9 +1,8 @@
-var precio = 10;
-var ingrediente1=1;
-var ingrediente2=1;
-var ingrediente3=1;
-var ingrediente4=3;
-var ingrediente5=5;
+var prize=13;                                       //valor de partida para calcular el precio en base a los botones activados al recargar
+$("strong").text("$"+prize);                        //imprime por pantalla el precio en base a los botones pulsados
+
+$(".btn-crust").removeClass("active");              //desactiva los botones crust y sauce de forma predefinida
+$(".btn-sauce").removeClass("active");
 
 $('.btn-pepperonni').click(function(event){ //fade de pepperonni en la lista de la derecha al pulsar el botón
         var pepperonni = $('li')[5];
@@ -11,6 +10,12 @@ $('.btn-pepperonni').click(function(event){ //fade de pepperonni en la lista de 
         $('.pep').fadeToggle();
 
         $('.btn-pepperonni').toggleClass("active");                 //Activa/desactiva el bottón, mejorar con CurrentTarget
+
+        if($("button.btn-pepperonni").hasClass("active")===true) {  //condicional que se repite para calcular el precio en base a la clase active
+        prize=prize+1;
+        }else{prize=prize-1;}
+
+        return $("strong").text("$"+prize);
 
       });
 
@@ -20,6 +25,12 @@ $('.btn-mushrooms').click(function(){ //fade de mushrooms en la lista de la dere
         $('.mushroom').fadeToggle();
 
         $('.btn-mushrooms').toggleClass("active");                 //Activa/desactiva el bottón, mejorar con CurrentTarget
+
+        if($("button.btn-mushrooms").hasClass("active")===true) {
+        prize=prize+1;
+        }else{prize=prize-1;}
+
+        return $("strong").text("$"+prize);
   
       });
 
@@ -31,6 +42,12 @@ $('.btn-green-peppers').click(function(){ //fade de green-peppers en la lista de
 
         $('.btn-green-peppers').toggleClass("active");              //Activa/desactiva el bottón, mejorar con CurrentTarget
 
+        if($("button.btn-green-peppers").hasClass("active")===true) {
+        prize=prize+1;
+        }else{prize=prize-1;}
+
+        return $("strong").text("$"+prize);
+
 
       });
 
@@ -40,6 +57,12 @@ $('.btn-sauce').click(function(){ //fade de sauce en la lista de la derecha al p
         $('.sauce').toggleClass('sauce-white');
 
         $('.btn-sauce').toggleClass("active");                      //Activa/desactiva el bottón, mejorar con CurrentTarget
+
+        if($("button.btn-sauce").hasClass("active")===true) {
+        prize=prize+3;
+        }else{prize=prize-3;}
+
+        return $("strong").text("$"+prize);
 
 
       });
@@ -51,17 +74,18 @@ $('.btn-crust').click(function(){ //fade de pepperonni en la lista de la derecha
 
         $('.btn-crust').toggleClass("active");                       //Activa/desactiva el bottón, mejorar con CurrentTarget
 
+        if($("button.btn-crust").hasClass("active")===true) {
+        prize=prize+5;
+        }else{prize=prize-5;}
+
+        return $("strong").text("$"+prize);
+
 
       });
 
 
 
-
-      precio=precio+ingrediente1+ingrediente2+ingrediente3+ingrediente4+ingrediente5;
-      $("strong").text("$"+precio);
-
-
-
+     
 
 
 
